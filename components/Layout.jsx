aa233ede57478fd459799a1ff0dc1157fb71d29f
Yofré López <c.yofrelopez@gmail.com>
2023-02-22 12:23:01 -0500

    agregué meta tag para wp

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -15,10 +15,16 @@ export const Layout = ({children}) => {
             <title>Semana Turística</title>
             <meta name="description" content="XXIX Semana Turística - Barranca 2023" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+
+            <meta property="og:type" content="website"></meta>
             <meta property = 'og:description' content= 'Cronograma de Actividades de la XXIX Semana Turística de Barranca'/>
-            <meta property = 'og:image' content='/images/fb_image.jpg'/>
+            <meta property = 'og:image' itemprop="image" content='/images/fb_image.jpg'/>
             <meta property = 'og:url' content='https://semana-turistica-2023.vercel.app/'/>
             <meta property = 'og:site_name' content='XXIX Semana Turística'/>
+            <meta property= "og:title" content="XXIX Semana Turística"></meta>
+
+            
             <link rel="icon" href="/fav.ico" />
 
 
