71183a507066cc574a4a103edce553596bb4ebda
Yofré López <c.yofrelopez@gmail.com>
2023-02-22 12:49:04 -0500

    Arreglé meta tag para SEO en redes sociales

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -17,12 +17,13 @@ export const Layout = ({children}) => {
             <meta name="viewport" content="width=device-width, initial-scale=1" />
 
 
-            <meta property="og:type" content="website"></meta>
+            <meta property="og:type" content="website" />
             <meta property = 'og:description' content= 'Cronograma de Actividades de la XXIX Semana Turística de Barranca'/>
+            <meta property="og:image:type" content="image/jpeg" />
             <meta property = 'og:image' itemprop="image" content='/images/fb_image.jpg'/>
             <meta property = 'og:url' content='https://semana-turistica-2023.vercel.app/'/>
             <meta property = 'og:site_name' content='XXIX Semana Turística'/>
-            <meta property= "og:title" content="XXIX Semana Turística"></meta>
+            <meta property= "og:title" content="XXIX Semana Turística" />
 
             
             <link rel="icon" href="/fav.ico" />
